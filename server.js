506f46fc3c137a6a0c81b2c4f82ddf4bcf8c34db
Ashish Dubey <ashish.dubey91@gmail.com>
2012-02-16 14:09:00 +0530

    Writing client/server cues

--- server.js
+++ server.js
@@ -2,36 +2,53 @@
 Much based on the Google Wave's implementation of operational transformation.
 */
 
-var clientSpaces, serverSpace, transacHistory;
+var clientSpaces, serverSpace, transacHistory, users;
 function xform(client, server) {
   //TODO::xform function
 }
 
 function transform(clientTransac) {
-  parentHash = clientTransac['parent'];
+  var parentHash = clientTransac['parent'];
   for( i = 0; !transacHistory[i]; i++ ) {
     if( parentHash == transacHistory[i]['parent'] ) {
-      serverTransac = transacHistory[i];
+      var serverTransac = transacHistory[i];
       break;
     }
   }
   var pair = xform(clientTransac, serverTransac);
-  transClient = pair.client;
-  transServer = pair.server;
+  var transClient = pair.client;
+  var transServer = pair.server;
   /*TODO::transform function??
   5- applyTransac(serverSpace, transClient)
   */
   return transClient;
 }
+
+function pushUser(user, socket) {
+  users.push(user);
+  socket.emit('user', user); //server acknowledgement for init?
+  socket.broadcast.emit('user', user);
+}
+
+function pushTransac(transac, socket) {
+  //var xformedTransac = transform(transac);
+  socket.emit('transaction', xformedTransac); // this is how it acknowledges the client
+  socket.broadcast.emit('transaction', xformedTransac);
+}
+
 var io = require('socket.io').listen(8080);
 io.sockets.on('connection', function(socket) {
   //maybe do something on new client?
 
-  socket.on('transaction', function(transaction) {
-    console.log(transaction);
-    //do something with the arrived transaction
-    xformedTransac = transform(transaction);
-    socket.emit('transaction', xformed_transac); // this is how it acknowledges the client
-    socket.broadcast.emit('transaction',xformed_transac);
+  socket.on('user', function(user) {
+    console.log('new user: ' + user);
+    newUser(user, socket);
   });
+
+  socket.on('transaction', function(transaction) {
+    console.log('new transaction: ' + JSON.stringify(transaction));
+    //do something with the arrived transaction
+    pushTransac(transaction);
+  });
+
 });
